1121d03e13029cc216aeffb6ba27f503570de10b
bsnacks000 <bsnacks000@gmail.com>
2021-08-12 09:58:15 -0400

    reworked the estimator and added skl smoke tests

--- setup.py
+++ setup.py
@@ -6,29 +6,28 @@ import os
 
 from setuptools import find_packages, setup
 
-# get __version__ from _version.py
+# get VERSION from _version.py
 ver_file = os.path.join('curvefit', '_version.py')
 with open(ver_file) as f:
-    exec(f.read())
+    version={}
+    exec(f.read(), version)
 
 DISTNAME = 'curvefit'
 DESCRIPTION = 'A sckit-learn wrapper around scipy.optimize.curve_fit'
 with codecs.open('README.md', encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
 
-MAINTAINER = 'bsnacks000'
+MAINTAINER = 'cunybpl'
 
 MAINTAINER_EMAIL = ''
-URL = 'https://github.com/bsnacks000/curvefit-estimator.git'
+URL = 'https://github.com/cunybpl/curvefit-estimator.git'
 
 LICENSE = 'new BSD'
-DOWNLOAD_URL = 'https://github.com/bsnacks000/curvefit-estimator.git'
+DOWNLOAD_URL = 'https://github.com/cunybpl/curvefit-estimator.git'
 
-VERSION = __version__
 INSTALL_REQUIRES = [
-    'numpy', 
-    'scipy', 
-    'scikit-learn']
+    'scikit-learn>=0.24'
+]
 
 CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Intended Audience :: Developers',
@@ -40,7 +39,6 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Operating System :: POSIX',
                'Operating System :: Unix',
                'Operating System :: MacOS',
-               'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7', 
                'Programming Language :: Python :: 3.8']
 
@@ -56,11 +54,12 @@ setup(name=DISTNAME,
       description=DESCRIPTION,
       license=LICENSE,
       url=URL,
-      version=VERSION,
+      version=version['VERSION'],
       download_url=DOWNLOAD_URL,
       long_description=LONG_DESCRIPTION,
       zip_safe=False,  # the package can run out of an .egg file
       classifiers=CLASSIFIERS,
-      packages=find_packages(),
+      packages=find_packages(exclude=["tests"]),
       install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+      extras_require=EXTRAS_REQUIRE
+    )
--- tests/test_curvefit.py
+++ tests/test_curvefit.py
@@ -5,17 +5,44 @@ from sklearn.datasets import load_iris
 from sklearn.utils._testing import assert_array_equal
 from sklearn.utils._testing import assert_allclose
 
-from curvefit import CurvefitEstimator
+from curvefit.estimator import CurvefitEstimator
 import unittest
 
-tc = unittest.TestCase()
+from sklearn import pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 
 
 def test_curvefit_estimator_against_scipy_example():
     # Use the scipy example to fit a function. 
 
     def func(x, a, b, c): 
-        return a * np.exp(-b * x) + c 
+        return (a * np.exp(-b * x) + c).squeeze() 
+
+    np.random.seed(1729)
+
+    xdata = np.linspace(0, 4, 50)
+    y = func(xdata, 2.5, 1.3, 0.5)
+    y_noise = 0.2 * np.random.normal(size=xdata.size)
+    ydata = y + y_noise
+
+
+    estimator = CurvefitEstimator(model_func=func)
+    estimator.fit(xdata.reshape(-1, 1), ydata)
+    expected = [
+        2.5542373783987986, 
+        1.3519104048478578, 
+        0.47450642597289794
+    ]
+    assert list(expected) == list(estimator.popt_)
+    estimator.predict(xdata.reshape(-1,1))    # more or less just a smoke test
+    
+
+def test_curvefit_estimator_with_pipeline_api():
+    
+    # smoke test to make sure we are compatible with sklearn pipeline API
+    def func(x, a, b, c): 
+        return (a * np.exp(-b * x) + c).squeeze() 
 
     np.random.seed(1729)
 
@@ -25,20 +52,35 @@ def test_curvefit_estimator_against_scipy_example():
     ydata = y + y_noise
 
     estimator = CurvefitEstimator(model_func=func)
-    estimator.fit(xdata.reshape(-1, 1), ydata, squeeze_X=True)
-    expected = [
-        2.5542373783987986, 
-        1.3519104048478578, 
-        0.47450642597289794
-    ]
-    assert list(expected) == list(estimator.popt_)
+    estimators = [('ct', StandardScaler()), ('f', estimator)]
 
-    predicted_res = estimator.predict(xdata)
-    print(predicted_res)
+    pipe = pipeline.Pipeline(estimators)
+    pipe.fit(xdata.reshape(-1, 1), ydata)
+    pipe.predict(xdata.reshape(-1,1)) 
+
+
+def test_curvefit_grid_search(): 
+    # smoketest that estimator works in a grid search
+
+    def f(x, a, b, c): 
+        return (a * np.exp(-b * x) + c).squeeze() 
     
+    def g(x, a, b, c, d): 
+        return (a * np.exp(-b * x) + c - d).squeeze()
 
-def test_curvefit_estimator_with_pipeline_api():
-    raise NotImplementedError 
+    # fake data
+    np.random.seed(1729)
 
+    xdata = np.linspace(0, 4, 50)
+    y = f(xdata, 2.5, 1.3, 0.5)
+    y_noise = 0.2 * np.random.normal(size=xdata.size)
+    ydata = y + y_noise
 
-    
+    params = {
+        'model_func': [f, g],
+    }
+
+    search = GridSearchCV(CurvefitEstimator(), param_grid=params)
+    search.fit(xdata.reshape(-1, 1), ydata)
+    assert search.best_estimator_.name_ == 'f'
+
